9e937c0fc71ffe27a98baba54e102036d3543f2c
Stefano Peron <abdita.imago@gmail.com>
2021-03-19 19:52:29 +0100

    split script for after boot

--- install_3_main.sh
+++ install_3_main.sh
@@ -39,72 +39,35 @@ echo $INST_USER:password | chpasswd
 
 echo "$INST_USER ALL=(ALL) ALL" >> /etc/sudoers.d/$INST_USER
 
-#TODO log as normal user
-
-
-
 ## Network manager
-sudo systemctl disable systemd-networkd.service
-sudo systemctl disable systemd-resolved.service
-sudo pacman -S --noconfirm --needed networkmanager network-manager-applet
-sudo systemctl enable NetworkManager
+systemctl disable systemd-networkd.service
+systemctl disable systemd-resolved.service
+pacman -S --noconfirm --needed networkmanager network-manager-applet
+systemctl enable NetworkManager
 
 ## Basic Desktop
-sudo pacman -S --noconfirm --needed avahi reflector pipewire pipewire-alsa pipewire-pulse pipewire-jack xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils bluez bluez-utils cups hplip
-sudo systemctl enable bluetooth
-sudo systemctl enable avahi-daemon
-sudo systemctl enable cups
+# pacman -S --noconfirm --needed avahi reflector pipewire pipewire-alsa pipewire-pulse pipewire-jack xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils bluez bluez-utils cups hplip
+# systemctl enable bluetooth
+# systemctl enable avahi-daemon
+# systemctl enable cups
 
 ## uncomment for basic GNOME
-# sudo pacman -S --noconfirm --needed gnome gnome-extra firefox materia-gtk-theme papirus-icon-theme
-# sudo systemctl enable gdm
+# pacman -S --noconfirm --needed gnome gnome-extra firefox materia-gtk-theme papirus-icon-theme
+# systemctl enable gdm
 
 ## uncomment for basic KDE
-# sudo pacman -S --noconfirm --needed plasma-meta kde-applications-meta firefox materia-kde papirus-icon-theme
-# sudo systemctl enable sddm
+# pacman -S --noconfirm --needed plasma-meta kde-applications-meta firefox materia-kde papirus-icon-theme
+# systemctl enable sddm
 
 ## laptop
-sudo pacman -S --noconfirm --needed iwd acpi acpi_call tlp acpid
-sudo systemctl enable tlp
-sudo systemctl enable acpid
+pacman -S --noconfirm --needed iwd acpi acpi_call tlp acpid
+systemctl enable tlp
+systemctl enable acpid
 
 ## kvm, qemu, libvirt
-sudo pacman -S --noconfirm --needed virt-manager qemu qemu-arch-extra bridge-utils vde2 ovmf
-sudo usermod -aG libvirt $INST_USER
-sudo systemctl enable libvirtd
-
-
-echo "nobody ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/nobody
-
-mkdir /opt/aurbuild
-chgrp nobody /opt/aurbuild
-chmod g+ws /opt/aurbuild
-setfacl -m u::rwx,g::rwx /opt/aurbuild
-setfacl -d --set u::rwx,g::rwx,o::- /opt/aurbuild
-
-cd /opt/aurbuild
-
-## add aur helper
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur/
-sudo -u nobody makepkg -si --noconfirm
-
-cd
-
-#remove aur build
-rm -fr /opt/aurbuild
-
-#remove nobody sudoers
-rm /etc/sudoers.d/nobody
-
-
-
-## tuxedo laptop
-pikaur -S --noconfirm linux-headers tuxedo-keyboard tuxedo-control-center
-
-## arch-x-icons-theme
-pikaur -S --noconfirm arc-x-icons-theme
-
+pacman -S --noconfirm --needed virt-manager qemu qemu-arch-extra bridge-utils vde2 ovmf
+usermod -aG libvirt $INST_USER
+systemctl enable libvirtd
 
 mkinitcpio -P
 
