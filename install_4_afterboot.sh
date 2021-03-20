247f0a6b1e3a12fbb714806798a77ce63d2c4893
Stefano Peron <abdita.imago@gmail.com>
2021-03-20 17:37:27 +0100

    fix afterboot script

--- install_4_afterboot.sh
+++ install_4_afterboot.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
+# default values
+INST_USER=xon
+INST_PWD=password
+
 ## Basic Desktop
-# pacman -S --noconfirm --needed avahi reflector pipewire pipewire-alsa pipewire-pulse pipewire-jack jack2 xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils bluez bluez-utils cups hplip
-# systemctl enable bluetooth
-# systemctl enable avahi-daemon
-# systemctl enable cups
+sudo pacman -S --noconfirm --needed avahi reflector pipewire pipewire-alsa pipewire-pulse pipewire-jack jack2 xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils bluez bluez-utils cups hplip
+sudo systemctl enable bluetooth
+sudo systemctl enable avahi-daemon
+sudo systemctl enable cups
 
 ## uncomment for basic GNOME
-# pacman -S --noconfirm --needed gnome gnome-extra firefox materia-gtk-theme papirus-icon-theme
-# systemctl enable gdm
+sudo pacman -S --noconfirm --needed gnome gnome-extra firefox materia-gtk-theme papirus-icon-theme
+sudo systemctl enable gdm
 
 ## uncomment for basic KDE
-# pacman -S --noconfirm --needed plasma-meta kde-applications-meta firefox materia-kde papirus-icon-theme
-# systemctl enable sddm
+sudo pacman -S --noconfirm --needed plasma-meta kde-applications-meta firefox materia-kde papirus-icon-theme
+sudo systemctl enable sddm
 
 ## laptop
-# pacman -S --noconfirm --needed iwd acpi acpi_call tlp acpid
-# systemctl enable tlp
-# systemctl enable acpid
+sudo pacman -S --noconfirm --needed iwd acpi acpi_call tlp acpid
+sudo systemctl enable tlp
+sudo systemctl enable acpid
 
 ## kvm, qemu, libvirt
-# pacman -S --noconfirm --needed virt-manager qemu qemu-arch-extra bridge-utils vde2 ovmf
-# usermod -aG libvirt $INST_USER
-# systemctl enable libvirtd
+sudo pacman -S --noconfirm --needed virt-manager qemu qemu-arch-extra bridge-utils vde2 ovmf
+sudo usermod -aG libvirt $INST_USER
+sudo systemctl enable libvirtd
 
 ## add aur helper
-# git clone https://aur.archlinux.org/pikaur.git
-# cd pikaur/
-# sudo makepkg -si --noconfirm
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur/
+makepkg -si --noconfirm
 
 ## tuxedo laptop
-# pikaur -S --noconfirm --needed linux-headers tuxedo-keyboard tuxedo-control-center
+pikaur -S --noconfirm --needed linux-headers tuxedo-keyboard tuxedo-control-center
 
 ## arch-x-icons-theme
-# pikaur -S --noconfirm --needed arc-x-icons-theme
+pikaur -S --noconfirm --needed arc-x-icons-theme
 
-mkinitcpio -P
+sudo mkinitcpio -P
 
-/bin/echo -e "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+/bin/echo -e "\e[1;32mDone!\e[0m"
