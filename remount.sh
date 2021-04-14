f7c97dd49dab091dc8004382de30e91b340109de
ikerios <abdita.imago@gmail.com>
2021-04-14 18:29:30 +0200

    Update remount.sh

--- remount.sh
+++ remount.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-cryptsetup open /dev/nvme0n1p2 cryptroot
+cryptsetup open /dev/nvme0n1p3 cryptroot
 mount -o noatime,compress=zstd,space_cache=v2,subvol=@ /dev/mapper/cryptroot /mnt
 mount -o noatime,compress=zstd,space_cache=v2,subvol=@home /dev/mapper/cryptroot /mnt/home
 mount -o noatime,compress=zstd,space_cache=v2,subvol=@snapshots /dev/mapper/cryptroot /mnt/.snapshots
 mount -o noatime,compress=zstd,space_cache=v2,subvol=@var_log /dev/mapper/cryptroot /mnt/var/log
 mount /dev/nvme0n1p1 /mnt/efi
 
+cryptsetup open /dev/nvme0n1p2 cryptswap
+swapon /dev/mapper/cryptswap
 
 #grub-install --recheck --target=x86_64-efi --efi-directory=/efi --bootloader-id=Arch
 #grub-mkconfig -o /boot/grub/grub.cfg
